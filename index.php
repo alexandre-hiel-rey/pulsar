5ab2c8ab67f2cbca458c04b08f7e755fbf1f3a14
alexandre-hiel-rey <hielreyalexandre@gmail.com>
2021-06-15 10:55:01 +0200

    Arbo site

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+  <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+    <meta name="referrer" content="origin-when-cross-origin">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="msapplication-tap-highlight" content="no" />
+    <link rel="icon" type="image/svg" href="assets/media/images/building.svg" />
+    <title></title>
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+
+<body>
+
+</body>
+</html>
+<script src="https://kit.fontawesome.com/ea8e16aa74.js" crossorigin="anonymous"></script>
+<script src="assets/js/script.js"></script>
